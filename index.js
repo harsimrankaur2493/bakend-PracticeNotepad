73dd165b088f6492c31f722996d24913546de226
harsimrankaur2493 <harsimrankaur2493@gmail.com>
2025-01-14 18:18:04 +0530

    half way through

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express")
+const fs=require("fs")
 const app = express();
+
 
 app.set("view engine" , "ejs");
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.get("/" , (req, res)=>{
-    res.render("index")
+    fs.readdir(`./files` , (error , files)=>{
+        res.render(`index` , {files: files})
+    })
+    
+});
+
+app.post("/create" , (req, res)=>{
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt` , req.body.description , (error)=>{
+        res.redirect("/");
+    })
 })
+
 
 app.listen(3000)
